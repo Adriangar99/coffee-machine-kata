84fa384afb6403838815ca38f5eac3b59d3de8aa
Adrián García <adriangargar99@gmail.com>
2024-05-24 17:32:37 +0200

    add drink types

--- src/main/kotlin/coffeemachine/CoffeeMachine.kt
+++ src/main/kotlin/coffeemachine/CoffeeMachine.kt
@@ -1,13 +1,31 @@
 package coffeemachine
 
-class CoffeeMachine(private val drinkMaker: DrinkMaker) {
-    fun selectCoffee(){
+enum class DrinkType(val value: String) {
+    COFFEE("C"),
+    TEA("T"),
+    CHOCOLATE("H")
+}
 
+open class CoffeeMachine(private val drinkMaker: DrinkMaker) {
+    lateinit var drinkType: DrinkType
+        private set
+
+    var sugarQuantity: Int = 0
+        private set
+
+    fun select(selectedDrink: DrinkType) {
+        drinkType = selectedDrink
     }
-    // selectChocolate()
-    // selectTea()
-    // addSugar()
-    fun makeDrink(){
 
+    fun addSugar() {
+        sugarQuantity += 1
+    }
+
+    fun makeDrink() {
+        drinkMaker.invoke(buildCommand())
+    }
+
+    private fun buildCommand(): String {
+        return "${drinkType.value}::"
     }
 }
--- src/test/kotlin/coffeemachine/CoffeeMachineTest.kt
+++ src/test/kotlin/coffeemachine/CoffeeMachineTest.kt
@@ -2,22 +2,60 @@ package coffeemachine
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.mock
+import org.mockito.Mockito.mock
 import org.mockito.kotlin.verify
 
 class CoffeeMachineTest {
 
-    private val drinkMaker: DrinkMaker = mock()
+    private val drinkMaker = mock<DrinkMaker>()
 
     @Test
     fun `Given a CoffeeMachine when coffee is selected it should print coffee`() {
         val coffeeMachine = CoffeeMachine(drinkMaker)
-        coffeeMachine.selectCoffee()
+
+        coffeeMachine.select(DrinkType.COFFEE)
         coffeeMachine.makeDrink()
 
-        verify(1) {
-            drinkMaker.invoke("C::")
-        }
+        verify(drinkMaker).invoke("C::")
+    }
 
+    @Test
+    fun `Given a CoffeeMachine when coffee is selected and sugar is added it should print coffee and sugar`() {
+        val coffeeMachine = CoffeeMachine(drinkMaker)
+
+        coffeeMachine.select(DrinkType.COFFEE)
+        coffeeMachine.addSugar()
+        coffeeMachine.makeDrink()
+
+        verify(drinkMaker).invoke("C:1:0")
+    }
+
+    @Test
+    fun `Given a CoffeeMachine when tea is selected it should print tea`() {
+        val coffeeMachine = CoffeeMachine(drinkMaker)
+
+        coffeeMachine.select(DrinkType.TEA)
+        coffeeMachine.makeDrink()
+
+        verify(drinkMaker).invoke("T::")
+    }
+
+    @Test
+    fun `Given a CoffeeMachine when chocolate is selected it should print chocolate`() {
+        val coffeeMachine = CoffeeMachine(drinkMaker)
+
+        coffeeMachine.select(DrinkType.CHOCOLATE)
+        coffeeMachine.makeDrink()
+
+        verify(drinkMaker).invoke("H::")
+    }
+
+    @Test
+    fun `Given a CoffeeMachine when select coffee it should save it`() {
+        val coffeeMachine = CoffeeMachine(drinkMaker)
+
+        coffeeMachine.select(DrinkType.COFFEE)
+
+        assertThat(coffeeMachine.drinkType).isEqualTo(DrinkType.COFFEE)
     }
 }
